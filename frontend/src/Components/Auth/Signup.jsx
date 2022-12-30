db39879f8c86f9a0ab1000c03da71cefd4b33a36
aman-109 <aman.mate.52@gmail.com>
2022-12-30 21:49:11 +0530

    signup form frontend done

---
+++ frontend/src/Components/Auth/Signup.jsx
@@ -0,0 +1,110 @@
+import { Button, FormControl, FormLabel, Input, InputGroup, InputRightElement, Stack, VStack } from '@chakra-ui/react'
+import React, { useState } from 'react'
+import {ViewIcon,ViewOffIcon} from "@chakra-ui/icons"
+
+const Signup = () => {
+  const [state,setState]=useState({
+    name:"",
+    email:"",
+    password:"",
+    confirmPassword:"",
+
+  })
+  const [profile,setProfile]=useState()
+  const [show,setShow]=useState(false)
+
+  const handleChange=(e)=>{
+    const {name,value}=e.target
+    setState({
+      ...state,
+      [name]:value
+    })
+  }
+  const postProfilePic=(pic)=>{
+
+  }
+
+  const handleSubmit=(e)=>{
+    e.preventDefault()
+  }
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+    <VStack spacing='5px'>
+      <FormControl id="first_name" isRequired>
+        <FormLabel>Name</FormLabel>
+        <Input
+        type="text"
+        placeholder='Enter Your Name'
+        name="name"
+        value={state.name}
+        onChange={handleChange}
+        />
+      </FormControl>
+      <FormControl id="email" isRequired>
+        <FormLabel>Email</FormLabel>
+        <Input
+        type="email"
+        placeholder='Enter Your Email'
+        name="email"
+        value={state.email}
+        onChange={handleChange}
+        />
+      </FormControl>
+      <FormControl id="password" isRequired>
+        <FormLabel>Password</FormLabel>
+        <Input
+        type="password"
+        placeholder='Enter Your Password'
+        name="password"
+        value={state.password}
+        onChange={handleChange}
+        /> 
+      </FormControl>
+      
+      <FormControl id="confirm_password" isRequired>
+        <FormLabel>Confirm Password</FormLabel>
+        <InputGroup>
+        <Input
+        type={show?"text":"password"}
+        placeholder='Confirm Your Password'
+        name="confirmPassword"
+        value={state.confirmPassword}
+        onChange={handleChange}
+        />
+        <InputRightElement width="4.5rem">
+          <Button h="1.75rem" size="sm" variant="outline" border="none" onClick={()=>setShow(!show)}>
+              {!show ? <ViewOffIcon/> :<ViewIcon/>}
+          </Button>
+        </InputRightElement>
+        
+        </InputGroup>
+      </FormControl>
+
+      <FormControl id="pic" isRequired>
+        <FormLabel>Upload your Picture</FormLabel>
+        <Input
+        type="file"
+        p={1}
+        accept='image/*'
+        onChange={(e)=>postProfilePic(e.target.files[0])}
+        /> 
+      </FormControl>
+      <Stack>
+
+      <Button colorScheme={"blue"}
+      width="100%"
+      // mt={15}
+    type="submit"
+      >
+        Sign Up
+      </Button>
+      </Stack>
+      
+    </VStack>
+      </form>
+      </>
+  )
+}
+
+export default Signup
--- frontend/src/Pages/Homepage.jsx
+++ frontend/src/Pages/Homepage.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import {
+  Box,
+  Container,
+  Text,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+} from "@chakra-ui/react";
+import React from "react";
+import Login from "../Components/Auth/Login";
+import Signup from "../Components/Auth/Signup";
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
-  )
-}
+    <Container maxW="xl" textAlign="center" centerContent>
+      {/* App Title */}
+      <Box
+        p={3}
+        bg="white"
+        w="100%"
+        m="40px 0 15px 0"
+        justifyContent="center"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text fontSize="4Xl" fontFamily="Open Sans" color="black">
+          I-We-Chat
+        </Text>
+      </Box>
+      {/* Login Signup Tabs */}
+      <Box bg="white" w="100%" p={4} color="black" borderRadius="lg" borderWidth="1px">
+        <Tabs variant="enclosed">
+          <TabList mb="1em">
+            <Tab width="50%">Login</Tab>
+            <Tab width="50%">Signup</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              {/* login */}
+              <Login/>
+            </TabPanel>
+            <TabPanel>
+             {/* signup */}
+             <Signup/>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
+};
 
-export default Homepage
+export default Homepage;
